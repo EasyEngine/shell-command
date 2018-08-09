cde3d30c8c72d8b00f6c02c48ffc12db75b56658
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2018-08-09 11:51:32 +0530

    Update functional calls acc to refactoring
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

--- src/Shell_Command.php
+++ src/Shell_Command.php
@@ -24,16 +24,17 @@ class Shell_Command extends EE_Command {
 	 * : Name of website to run shell on.
 	 */
 	public function __invoke( $args ) {
+
 		EE\Utils\delem_log( 'ee shell start' );
-		$args = EE\Utils\set_site_arg( $args, 'shell' );
+		$args      = EE\SiteUtils\auto_site_name( $args, 'shell', '' );
 		$site_name = EE\Utils\remove_trailing_slash( $args[0] );
 		if ( EE::db()::site_in_db( $site_name ) ) {
-			$db_select = EE::db()::select( ['site_path'], ['sitename' => $site_name]);
-			$site_root    = $db_select[0]['site_path'];
+			$db_select = EE::db()::select( [ 'site_path' ], [ 'sitename' => $site_name ] );
+			$site_root = $db_select[0]['site_path'];
 		} else {
 			EE::error( "Site $site_name does not exist." );
 		}
-		chdir($site_root);
+		chdir( $site_root );
 		$this->run( "docker-compose exec --user='www-data' php bash" );
 		EE\Utils\delem_log( 'ee shell end' );
 	}
@@ -43,9 +44,9 @@ class Shell_Command extends EE_Command {
 		if ( ! $descriptors ) {
 			$descriptors = array( STDIN, STDOUT, STDERR );
 		}
-		
+
 		$final_cmd = EE\Utils\force_env_on_nix_systems( $cmd );
-		$proc = EE\Utils\proc_open_compat( $final_cmd, $descriptors, $pipes );
+		$proc      = EE\Utils\proc_open_compat( $final_cmd, $descriptors, $pipes );
 		if ( ! $proc ) {
 			exit( 1 );
 		}
