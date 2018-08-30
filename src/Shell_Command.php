f9e1400518bbbb07fcd931415647f3bbfc476b62
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2018-08-30 18:43:23 +0530

    Add php container exsits check and update comments
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

--- src/Shell_Command.php
+++ src/Shell_Command.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * Executes wp-cli command on a site.
+ * Brings up a shell to run wp-cli, composer etc.
  *
  * ## EXAMPLES
  *
- *     # Create simple WordPress site
- *     $ ee wp test.local plugin list
+ *     # Open shell of example.com
+ *     $ ee shell example.com
  *
  * @package ee-cli
  */
@@ -30,13 +30,14 @@ class Shell_Command extends EE_Command {
 		$args      = auto_site_name( $args, 'shell', '' );
 		$site_name = EE\Utils\remove_trailing_slash( $args[0] );
 
-		$site = Site::find( $site_name, [ 'site_enabled', 'site_fs_path' ] );
+		$site = Site::find( $site_name );
 
 		if ( ! $site || ! $site->site_enabled ) {
 			EE::error( "Site $site_name does not exist or is not enabled." );
 		}
 
 		chdir( $site->site_fs_path );
+		$this->check_shell_available( 'php', $site );
 		$this->run( "docker-compose exec --user='www-data' php bash" );
 		EE\Utils\delem_log( 'ee shell end' );
 	}
@@ -61,6 +62,23 @@ class Shell_Command extends EE_Command {
 		$r = proc_close( $proc );
 		if ( $r ) {
 			exit( $r );
+		}
+	}
+
+	/**
+	 * Function to check if container supporting shell is present in docker-compose.yml or not.
+	 *
+	 * @param string $shell_container Container to be checked.
+	 * @param Object $site            Contains relevant site info.
+	 */
+	private function check_shell_available( $shell_container, $site ) {
+
+		$launch   = EE::launch( 'docker-compose config --services' );
+		$services = explode( PHP_EOL, trim( $launch->stdout ) );
+		if ( ! in_array( $shell_container, $services, true ) ) {
+			EE::debug( 'Site type: ' . $site->site_type );
+			EE::debug( 'Site command: ' . $site->app_sub_type );
+			EE::error( sprintf( '%s site does not have support to launch %s shell.', $shell_container, $site->site_url ) );
 		}
 	}
 
